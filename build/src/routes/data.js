b2412a642b07b960f657c78ba71c4e9f78d3991d
alu0101130675 <alu0101130675@ull.edu.es>
2023-04-14 15:40:29 +0100

    fix axe route name

--- build/src/routes/data.js
+++ build/src/routes/data.js
@@ -10,7 +10,7 @@ exports.dataRouter.post('/:collectionName', userExtractor_1.userExtractor, admin
 exports.dataRouter.get('/fileNames', data_1.getFileNames);
 exports.dataRouter.get('/configFiles', data_1.getConfigFileNames);
 exports.dataRouter.get('/configField/:name/:idFlag?', data_1.getConfigFile);
-exports.dataRouter.get('/configField/:name', data_1.getAxes);
+exports.dataRouter.get('/axes/:name', data_1.getAxes);
 exports.dataRouter.get('/dataFile/:name', data_1.getDataByFileName);
 exports.dataRouter.delete('/dataFile/:name/:id', userExtractor_1.userExtractor, adminCheck_1.adminCheck, data_1.deleteFiles);
 exports.dataRouter.put('/configFiles/:id', userExtractor_1.userExtractor, adminCheck_1.adminCheck, data_1.updateConfigFile);
